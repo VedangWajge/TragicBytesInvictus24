fa85be9506a5efab78aa2fc9aa2b720d64444487
Vedant Sanap <vedantsanap123@gmail.com>
2024-01-21 02:50:49 +0530

    bakchodi

--- test1.js
+++ test1.js
@@ -1,11 +1,26 @@
-function showOptions(element) {
-    const optionsWindow = element.querySelector('.options-window');
-    optionsWindow.style.display = 'block';
+let currentSlide = 0;
+
+function showSlide(index) {
+    const slides = document.getElementsByClassName("slide");
+
+    if (index >= slides.length) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = slides.length - 1;
+    } else {
+        currentSlide = index;
+    }
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slides[currentSlide].style.display = "block";
 }
 
-function hideOptions() {
-    const optionsWindows = document.querySelectorAll('.options-window');
-    optionsWindows.forEach(window => {
-        window.style.display = 'none';
-    });
+function changeSlide(n) {
+    showSlide(currentSlide += n);
 }
+
+// Initial display
+showSlide(currentSlide);
