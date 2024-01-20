e6f36297d64bc34e6556fea3f58bee027bb58957
Vedant Sanap <vedantsanap123@gmail.com>
2024-01-20 21:36:32 +0530

    bohot kuch kiya

---
+++ newINVIC24/JS files/index.js
@@ -0,0 +1,10 @@
+let registerBtn = document.getElementById('regId')
+let loginBtn = document.getElementById("logId")
+registerBtn.addEventListener("click", function() {
+    
+    window.location.href = "register.html";
+});
+loginBtn.addEventListener("click", function() {
+    
+    window.location.href = "login.html";
+});
